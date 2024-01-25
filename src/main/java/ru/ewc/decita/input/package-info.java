f580067d3eed8f4a2e63ea6a09d3173add8c0e5f
Евгений Терехов <nergal@psu.ru>
2024-01-25 07:56:30 +0400

    chore: added new puzzles

--- src/main/java/ru/ewc/decita/input/package-info.java
+++ src/main/java/ru/ewc/decita/input/package-info.java
@@ -33,3 +33,11 @@ package ru.ewc.decita.input;
 // @todo #40 File system Decision Tables should be prototypes for every computation.
 // It means that computing something once doesn't change the state of all the prototypes and the
 // next computation will use clean, uncomputed Coordinates.
+
+// @todo #42 Implement TableSources class to handle the implicit locator names.
+// My idea is to allow short notation for coordinates in tables, like just "42" instead of
+// "constant::42" for constants (omitting the "constant" locator name) or "is_expired" instead of
+// "is_expired::outcome" for conditions (omitting the outcome name for the table with a single
+// outcome). This can be made possible with the help of the intermediary object holding all the
+// RawContents (thus knowing all the table names) and able to derive FQN for coordinates from the
+// short notation.

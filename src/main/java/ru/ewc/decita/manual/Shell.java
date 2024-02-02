d98cacde5c8677978823acfbfc2d51a6fadb99a5
Евгений Терехов <nergal@psu.ru>
2024-02-02 19:08:19 +0400

    #66 feature: rebuild completer after pointing to a tables folder

--- src/main/java/ru/ewc/decita/manual/Shell.java
+++ src/main/java/ru/ewc/decita/manual/Shell.java
@@ -26,6 +26,9 @@ package ru.ewc.decita.manual;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import org.jline.reader.LineReader;
 import org.jline.reader.LineReaderBuilder;
 import org.jline.reader.ParsedLine;
@@ -33,6 +36,7 @@ import org.jline.reader.impl.DefaultParser;
 import org.jline.reader.impl.completer.StringsCompleter;
 import org.jline.terminal.Terminal;
 import org.jline.terminal.TerminalBuilder;
+import ru.ewc.decita.input.PlainTextContentReader;
 
 /**
  * I am the shell for manual library testing. My main responsibility is to hide Java complexities
@@ -42,9 +46,19 @@ import org.jline.terminal.TerminalBuilder;
  */
 public final class Shell {
     /**
+     * Set of predefined autocompletion options, contains all available commands.
+     */
+    public static final List<String> COMMANDS = List.of("sources", "decide");
+
+    /**
+     * Object holding current terminal.
+     */
+    private final Terminal terminal;
+
+    /**
      * Object that reads the user's input.
      */
-    private final LineReader reader;
+    private LineReader reader;
 
     /**
      * Object that can write anything to console.
@@ -63,10 +77,11 @@ public final class Shell {
      */
     @SuppressWarnings("PMD.ConstructorOnlyInitializesOrCallOtherConstructors")
     private Shell() throws IOException {
-        final Terminal terminal = TerminalBuilder.terminal();
-        this.writer = terminal.writer();
-        this.reader = LineReaderBuilder.builder()
-            .terminal(terminal)
+        this.terminal = TerminalBuilder.terminal();
+        this.writer = this.terminal.writer();
+        this.reader = LineReaderBuilder
+            .builder()
+            .terminal(this.terminal)
             .completer(new StringsCompleter("sources", "hello"))
             .parser(new DefaultParser())
             .build();
@@ -77,8 +92,6 @@ public final class Shell {
             )
         );
     }
-    // @todo #61 Rebuild Completer each time the folder with tables is loaded.
-    // That will allow autocompletion for table names, so it will be easier to issue commands.
 
     /**
      * The method to run a simplified manual testing utility.
@@ -145,7 +158,35 @@ public final class Shell {
      */
     private void pointToSources(final String path) {
         this.folder = URI.create(String.format("file://%s", path.replace("'", "")));
+        final Set<String> tables = this.extractFilenamesFromPath();
+        this.reader = LineReaderBuilder
+            .builder()
+            .terminal(this.terminal)
+            .completer(new StringsCompleter(completionOptionsFor(tables)))
+            .build();
         this.writer.printf("Let's import tables from %s%n", path);
+    }
+
+    /**
+     * Returns a complete set of all the table names, read from the user-specified folder.
+     *
+     * @return Set of Strings, representing decision table names.
+     */
+    private Set<String> extractFilenamesFromPath() {
+        return new PlainTextContentReader(this.folder, ".csv", ";").allTables().keySet();
+    }
+
+    /**
+     * Computes a list of all autocompletion options base on a predefined set of available commands
+     * and a list of tables in a user-specified folder.
+     *
+     * @param tables List of table names.
+     * @return A collection of string elements, each of which is the autocomplete option.
+     */
+    private static List<String> completionOptionsFor(final Set<String> tables) {
+        final List<String> result = new ArrayList<>(tables);
+        result.addAll(Shell.COMMANDS);
+        return result;
     }
 
     /**

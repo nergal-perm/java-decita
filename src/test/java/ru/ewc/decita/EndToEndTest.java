e4e5a1c7477f571151b7e76608180acc42db14a9
Евгений Терехов <nergal@psu.ru>
2024-05-05 09:50:40 +0400

    chore: removed license duplicates

--- src/test/java/ru/ewc/decita/EndToEndTest.java
+++ src/test/java/ru/ewc/decita/EndToEndTest.java
@@ -22,30 +22,6 @@
  * SOFTWARE.
  */
 
-/*
- * MIT License
- *
- * Copyright (c) 2024 Eugene Terekhov
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package ru.ewc.decita;
 
 import java.net.URI;
@@ -80,13 +56,13 @@ final class EndToEndTest {
     void shouldComputeTheWholeTable() throws DecitaException {
         final Locators state = new Locators(
             Map.of(
-                "data", new TestLocator(
+                "data", new InMemoryLocator(
                     Map.of("is-stored", "true")
                 ),
-                "market", new TestLocator(
+                "market", new InMemoryLocator(
                     Map.of("shop", 2)
                 ),
-                "currentPlayer", new TestLocator(
+                "currentPlayer", new InMemoryLocator(
                     Map.of("name", "Eugene")
                 )
             )
@@ -106,13 +82,13 @@ final class EndToEndTest {
     void shouldComputeTheWholeTableWithElseRule() throws DecitaException {
         final Locators state = new Locators(
             Map.of(
-                "data", new TestLocator(
+                "data", new InMemoryLocator(
                     Map.of("is-stored", false)
                 ),
-                "market", new TestLocator(
+                "market", new InMemoryLocator(
                     Map.of("shop", 3)
                 ),
-                "currentPlayer", new TestLocator(
+                "currentPlayer", new InMemoryLocator(
                     Map.of("name", "Eugene")
                 )
             )
@@ -132,7 +108,7 @@ final class EndToEndTest {
     void shouldThrowIfSeveralRulesResolveToTrue() {
         final Locators state = new Locators(
             Map.of(
-                "data", new TestLocator(
+                "data", new InMemoryLocator(
                     Map.of("value", 1)
                 )
             )
--- src/test/java/ru/ewc/decita/InMemoryLocator.java
+++ src/test/java/ru/ewc/decita/InMemoryLocator.java
@@ -22,30 +22,6 @@
  * SOFTWARE.
  */
 
-/*
- * MIT License
- *
- * Copyright (c) 2024 Eugene Terekhov
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 package ru.ewc.decita;
 
 import java.util.Map;
@@ -56,7 +32,7 @@ import java.util.Map;
  *
  * @since 0.2
  */
-public final class TestLocator implements Locator {
+public final class InMemoryLocator implements Locator {
     /**
      * Simple key-value storage.
      */
@@ -67,7 +43,7 @@ public final class TestLocator implements Locator {
      *
      * @param storage The pre-filled key-value storage to start with.
      */
-    public TestLocator(final Map<String, Object> storage) {
+    public InMemoryLocator(final Map<String, Object> storage) {
         this.storage = storage;
     }
 
